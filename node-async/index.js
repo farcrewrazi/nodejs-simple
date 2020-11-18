8db7a1b761c7579e1986ed6a203ed7f98162b81b
farcrewrz <farcrewrz@gmail.com>
2020-11-18 16:30:40 +0800

    Done make node-async example

--- node-async/index.js
+++ node-async/index.js
@@ -1,1 +1,21 @@
-console.log('Init, Node Async mini project.')
+const fs = require('fs')
+const neatCSV = require('neat-csv')
+const _async = require('async')
+
+const filePath = './resource_data.csv'
+
+fs.readFile(filePath, async (err, data) => {
+    if(err){
+        console.error(err)
+        return
+    }
+
+    let csvData = await neatCSV(data)
+    _async.eachSeries(csvData, async(rowData, callback) => {
+        console.log(`${rowData.id} - ${rowData.first_name}, ${rowData.last_name}, ${rowData.email}, ${rowData.gender}, ${rowData.ip_address}`)
+        callback
+    }, () => {
+        console.log('All done perfectly.')
+        console.log('Then, do something here!!')
+    })
+})

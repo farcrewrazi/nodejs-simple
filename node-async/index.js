0b0b867387d3ff0d33310280f64eddabdd9f410b
farcrewrz <farcrewrz@gmail.com>
2020-11-18 16:34:45 +0800

    Added comments

--- node-async/index.js
+++ node-async/index.js
@@ -4,17 +4,22 @@ const _async = require('async')
 
 const filePath = './resource_data.csv'
 
+//Start read file first
 fs.readFile(filePath, async (err, data) => {
     if(err){
         console.error(err)
         return
     }
 
+    //Init array of data from csv
     let csvData = await neatCSV(data)
+
+    //Using async.eachseries to await all from csv data process until end
     _async.eachSeries(csvData, async(rowData, callback) => {
         console.log(`${rowData.id} - ${rowData.first_name}, ${rowData.last_name}, ${rowData.email}, ${rowData.gender}, ${rowData.ip_address}`)
         callback
     }, () => {
+        //Here is the function after the loop is complete.
         console.log('All done perfectly.')
         console.log('Then, do something here!!')
     })
